46d736073005bb86976b1118b366e584718c2753
hhanleeSwk <hhanlee@sweetk.co.kr>
2025-07-21 19:17:45 +0900

    [FE][Change] common lib 로딩되도록 수정

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,10 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      'packages/common': path.resolve(__dirname, '../packages/common'),
+    },
   },
 })

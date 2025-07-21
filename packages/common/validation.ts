d5bb23f1dd755bee50e190e14612489a72a1bc27
hhanleeSwk <hhanlee@sweetk.co.kr>
2025-07-21 16:05:14 +0900

    [COM][Change]  Validation 처리 추가

--- packages/common/validation.ts
+++ packages/common/validation.ts
@@ -3,7 +3,7 @@
 export function isValidEmail(email: string): boolean {
   return /^[\w.-]+@[\w.-]+\.[A-Za-z]{2,}$/.test(email);
 }
-
+ 
 export function isValidPassword(password: string): boolean {
   // 8자 이상, 영문/숫자/특수문자 포함
   return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+\[\]{};':"\\|,.<>/?]).{8,}$/.test(password);
